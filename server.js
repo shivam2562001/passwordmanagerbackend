dcd619b6168e9a894964bc8f6a450c1216ca895f
shivam2562001 <ssssss2562001@gmail.com>
2020-07-31 09:38:45 +0530

    fourth

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ const cors = require("cors");
 
  const app = express();
 
- app.use(cors());
+
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+//  app.use(cors());
 dotenv.config({ path: "./.env" });
 
 mongoose
